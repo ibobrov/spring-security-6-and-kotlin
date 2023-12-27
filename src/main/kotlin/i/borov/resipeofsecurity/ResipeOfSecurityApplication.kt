019dd8b4b5590cd2da447739cf6227d42226aa36
ibobrov <coderilya@gmail.com>
2023-12-27 16:27:53 +1000

    Initial commit

---
+++ src/main/kotlin/i/borov/resipeofsecurity/ResipeOfSecurityApplication.kt
@@ -0,0 +1,11 @@
+package i.borov.resipeofsecurity
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ResipeOfSecurityApplication
+
+fun main(args: Array<String>) {
+	runApplication<ResipeOfSecurityApplication>(*args)
+}
---
+++ src/test/kotlin/i/borov/resipeofsecurity/ResipeOfSecurityApplicationTests.kt
@@ -0,0 +1,13 @@
+package i.borov.resipeofsecurity
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ResipeOfSecurityApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
